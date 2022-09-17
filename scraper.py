fb70deb923ee949ed4bbeaa37f77b92d2819f392
Thomasorus <contact@thomasorus.com>
2022-09-17 21:17:01 +0200

    Added cache

--- scraper.py
+++ scraper.py
@@ -1,10 +1,25 @@
 import os
+import json
+import re
 import requests
 import shutil
 from bs4 import BeautifulSoup
+import string
 
+cache_path = "cache.json"
 path = "movies"
 url = "https://www.imdb.com/title/"
+
+cache_file = open(cache_path, "r")
+cache_text = cache_file.read()
+if cache_text.strip() == "":
+    print("New cache")
+    cache = False
+else:
+    cache = True
+    cache_content = json.loads(cache_text)
+cache_file.close()
+
 
 files = os.listdir(path)
 sort = sorted(files)
@@ -13,43 +28,100 @@ sort.reverse()
 filenames = []
 
 for file in sort:
-	filenames.append(file.replace(".txt", ""))
-
+    filenames.append(file.replace(".txt", ""))
 
 
 all_movies = []
+tmp_cache = []
 menu = "<ul>"
 for year in filenames:
-	print(year)
-	file = open(path + "/" + year + ".txt", "r")
-	movies_ids = file.read().split()
-	menu += "<li><a href='#" + year + "''>" + year + "</a></li>"
+    print("\n" + year)
+    file = open(path + "/" + year + ".txt", "r")
+    movies_ids = file.read().split("\n")
+    menu += "<li><a href='#" + year + "''>" + year + "</a></li>"
 
-	movies = []
-	for movie_id in movies_ids:
-		link = url + movie_id
-		res = requests.get(link)
-		soup = BeautifulSoup(res.content, 'html.parser')
-		title = soup.find('h1')
-		print(str(title.contents[0]))
-		image = soup.find('meta', property="og:image")
-		plot = soup.find('meta', property="og:description")
-		movie = {"title": str(title.contents[0]), "image": image.get("content"), "plot": plot.get("content"), "link": link}
-		movies.append(movie)
+    movies = []
+    for movie_infos in movies_ids:
+        if movie_infos == "":
+            continue
+        movie_arr = re.split(" ", movie_infos)
+        movie_id = movie_arr[0]
+        if len(movie_arr) >= 2:
+            podium = movie_arr[1]
+        else:
+            podium = ""
+        match = False
+        if cache:
+            for m in cache_content:
+                if movie_id == m["id"]:
+                    print("Cached: ", m["title"])
+                    match = True
+                    movies.append(m)
+                    tmp_cache.append(m)
+                    break
 
-	year_arr = {"year": str(year), "movies": movies}
-	all_movies.append(year_arr)
+        if match == False:
+            link = url + movie_id
+            res = requests.get(link)
+            soup = BeautifulSoup(res.content, "html.parser")
+            title = soup.find("h1")
+            print("Fetch: ", str(title.contents[0]))
+            image = soup.find("meta", property="og:image")
+            plot = soup.find("meta", property="og:description")
+            movie = {
+                "id": movie_id,
+                "title": str(title.contents[0]),
+                "image": image.get("content"),
+                "plot": plot.get("content"),
+                "link": link,
+                "year": year,
+                "podium": podium,
+            }
+            movies.append(movie)
+            tmp_cache.append(movie)
+        match = False
+    year_arr = {"year": str(year), "movies": movies}
+    all_movies.append(year_arr)
+
+
+cache_string = json.dumps(tmp_cache)
+new_cache = open(cache_path, "w")
+new_cache.write(cache_string)
+new_cache.close()
 
 menu += "</ul>"
 content = ""
-print(all_movies)
+# print(all_movies)
+
 for item in all_movies:
-	print(item["year"])
-	content += "<section><div><div class='stick'><img class='moustache' src='images/" + item["year"] + ".png' aria-hidden='true'/>\n\t<h2 id=" + item["year"] + ">" + item["year"] + "</h2></div></div><div class='movies'>\n"
-	for movie in item["movies"]:
-		movie_string = "\t<article class='flow'>\n\t\t<img loading='lazy' src='" + movie["image"] + "'' alt='" + movie["title"] + "'/>\n\t\t<h3>" + movie["title"] + "</h3>\n\t\t<p>" + movie["plot"] + "</p>\n\t\t<a href='" + movie["link"] + "'>IMDB</a>\n\t</article>\n"
-		content += movie_string
-	content += "</div></section>"
+    print(item["year"])
+    content += (
+        "<section><div><div class='stick'><img class='moustache' src='images/"
+        + item["year"]
+        + ".png' aria-hidden='true'/>\n\t<h2 id="
+        + item["year"]
+        + ">"
+        + item["year"]
+        + "</h2></div></div><div class='movies'>\n"
+    )
+    for movie in item["movies"]:
+        movie_string = (
+            "\t<article class='flow "
+            + movie["podium"]
+            + "'>\n\t\t<img loading='lazy' src='"
+            + movie["image"]
+            + "'' alt='"
+            + movie["title"]
+            + "'/>\n\t\t<h3>"
+            + movie["title"]
+            + "</h3>\n\t\t<p>"
+            + movie["plot"]
+            + "</p>\n\t\t<a href='"
+            + movie["link"]
+            + "'>IMDB</a>\n\t</article>\n"
+        )
+        content += movie_string
+    content += "</div></section>"
 
 
 start = open("partials/start.html", "r")
@@ -70,11 +142,9 @@ f.close()
 assets = os.listdir("images")
 os.makedirs("docs/images")
 if assets:
-	for asset in assets:
-	    asset = os.path.join("images/", asset)
-	    if os.path.isfile(asset):
-	        shutil.copy(asset, "docs/images/")
+    for asset in assets:
+        asset = os.path.join("images/", asset)
+        if os.path.isfile(asset):
+            shutil.copy(asset, "docs/images/")
 else:
     print("No assets found!")
-
-
